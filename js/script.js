b655bd0d32d6138328479656b64085d3ce54bc65
Joseph Lander <josephclander@gmail.com>
2021-02-19 17:32:10 +0000

    Add form submit validation fn

--- js/script.js
+++ js/script.js
@@ -87,3 +87,99 @@ paymentSelector.addEventListener('change', (e) => {
     else hideElement(payMethods[method]);
   }
 });
+
+/**
+ *
+ * Validation functions
+ */
+
+/**
+ * name check present
+ * no blanks or spaces
+ */
+const isValidName = (name) => {
+  const regex = /\S+/;
+  const isValid = regex.test(name);
+  if (!isValid) console.error('Name: No blanks or spaces');
+  return isValid;
+};
+
+/**
+ * valid email address
+ * format = word@word.com
+ */
+const isValidEmail = (email) => {
+  const regex = /\w+@\w+.com/;
+  const isValid = regex.test(email);
+  if (!isValid) console.error('Email: format must be word@word.com');
+  return isValid;
+};
+
+/**
+ * activity validation
+ *  1 or more activities checked
+ */
+const isValidActivities = () => {
+  const activities = document.querySelectorAll('#activities-box label input');
+  for (let i = 0; i < activities.length; i++) {
+    const activity = activities[i];
+    // immediately return true if one is checked
+    if (activity.checked) return true;
+  }
+  console.error('Activities: 1 or more must be checked');
+  return false;
+};
+
+/**
+ * credit card number validator
+ * 13 - 16 digit number with no dashes or spaces
+ */
+const isValidCreditCardNumber = (ccNumber) => {
+  const regex = /^\d{13,16}$/;
+  const isValid = regex.test(ccNumber);
+  if (!isValid)
+    console.error('Credit Card Number: 13-16 digits, no spaces or dashes');
+  return isValid;
+};
+
+/**
+ * zip code validator
+ * 5 digit number
+ */
+const isValidZip = (zip) => {
+  const regex = /^\d{5}$/;
+  const isValid = regex.test(zip);
+  if (!isValid) console.error('Credit Card Zip: Must be 5 digits');
+  return isValid;
+};
+
+/**
+ * cvv validator
+ * 3 digit number
+ */
+const isValidCVV = (cvv) => {
+  const regex = /^\d{3}$/;
+  const isValid = regex.test(cvv);
+  if (!isValid) console.error('Credit Card CVV: Must be 3 digits');
+  return isValid;
+};
+
+// form submission validation
+const form = document.querySelector('form');
+// input fields to validate
+const emailInput = document.querySelector('#email');
+const creditCardInput = document.querySelector('#cc-num');
+const zipcodeInput = document.querySelector('#zip');
+const cvvInput = document.querySelector('#cvv');
+// form submit listener
+form.addEventListener('submit', (e) => {
+  !isValidName(nameInput.value) ? e.preventDefault() : null;
+  !isValidEmail(emailInput.value) ? e.preventDefault() : null;
+  !isValidActivities() ? e.preventDefault() : null;
+  // checks for credit card options
+  if (paymentSelector.value === 'credit-card') {
+    !isValidCreditCardNumber(creditCardInput.value) ? e.preventDefault() : null;
+    !isValidZip(zipcodeInput.value) ? e.preventDefault() : null;
+    !isValidCVV(cvvInput.value) ? e.preventDefault() : null;
+  }
+});
