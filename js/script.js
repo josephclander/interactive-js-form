aaf25efffdcef768dee217f41e84d1754b23764d
Joseph Lander <josephclander@gmail.com>
2021-02-19 11:36:08 +0000

    Add option handling for tshirt color

--- js/script.js
+++ js/script.js
@@ -17,3 +17,23 @@ otherJobSelect.addEventListener('change', () => {
  * CHECK - the other input element will still have
  *          a value if you type and then hide it
  */
+
+// setup option handling for t-shirt colors
+const tshirtColor = document.querySelector('#color');
+tshirtColor.disabled = true;
+const designSelection = document.querySelector('#design');
+designSelection.addEventListener('change', () => {
+  // sufficient to always enable tshirtColor here
+  // as default option will be disabled
+  tshirtColor.disabled = false;
+  const options = tshirtColor.children;
+  // start at 1 to miss default select option
+  for (let i = 1; i < options.length; i++) {
+    const option = options[i];
+    const designChoice = designSelection.value;
+    const optionType = option.dataset.theme;
+    // show items if design and option type match
+    if (designChoice === optionType) option.hidden = false;
+    else option.hidden = true;
+  }
+});
