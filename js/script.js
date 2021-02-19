60d1fc7b96c45739b6bcac36ff8dd04de4d58b08
Joseph Lander <josephclander@gmail.com>
2021-02-19 10:13:58 +0000

    Add show/hide fn for 'other job' select

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 // focus to initial text field
 const nameInput = document.querySelector('#name');
 nameInput.focus();
+
+// show/hide job role entry for "other"
+const otherJobInput = document.querySelector('#other-job-role');
+otherJobInput.style = 'display: none';
+const otherJobSelect = document.querySelector('#title');
+otherJobSelect.addEventListener('change', () => {
+  if (otherJobSelect.value === 'other') {
+    otherJobInput.style = 'display: inline-block';
+  } else {
+    otherJobInput.style = 'display: none';
+  }
+});
+/**
+ * CHECK - the other input element will still have
+ *          a value if you type and then hide it
+ */
